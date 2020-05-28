b78d6c3bcbeb50a935aadeea4ce1b5afb141cf71
Myka Kugaya <mykakug129@gmail.com>
2020-05-27 19:33:58 -0700

    initial inquirer prompt with switch cases

--- index.js
+++ index.js
@@ -22,5 +22,49 @@ connection.connect(function(err) {
 });
 
 function init() {
-    
+    inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "Add new department",
+            "Add new role",
+            "Add new employee",
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Update employee role"
+        ]
+    })
+    .then(function(response) {
+        switch (response.action) {
+        case "Add new department":
+            addDepartment();
+            break;
+  
+        case "Add new role":
+            addRole();
+            break;
+  
+        case "Add new employee":
+            addEmployee();
+            break;
+  
+        case "View all departments":
+            viewDepartments();
+            break;
+
+        case "View all roles":
+            viewRoles();
+            break;
+
+        case "View all employees":
+            viewEmployees();
+            break;
+        case "Update employee role":
+            updateEmployeeRole();
+            break;
+        }
+    });
 }
